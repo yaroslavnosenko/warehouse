3d120bbb9c8e0ef12895185ae5f923643a299c26
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2021-12-15 22:44:36 +0100

    client: items table

--- client/src/components/item-list.tsx
+++ client/src/components/item-list.tsx
@@ -1,18 +1,38 @@
 import { useEffect, useState } from 'react'
-import { Box, Grid, HStack, IconButton, Skeleton, Text, Alert, AlertIcon } from '@chakra-ui/react'
+import { Grid, HStack, IconButton, Skeleton, Text, Alert, AlertIcon, Stack, StackDivider } from '@chakra-ui/react'
 import { BiTrash, BiDotsVerticalRounded, BiInfoCircle } from 'react-icons/bi'
 
 import { deleteItem, getAllItems, updateItem } from '../services'
 import { Item } from '../types'
 import { EditItemForm } from '.'
 
+const TableHead = () => {
+  return (
+    <Grid templateColumns="2fr 1fr 1fr 1fr 8rem" alignItems="center" color="gray.500">
+      <Text fontWeight="500" mr="4" flex="1">
+        Title
+      </Text>
+      <Text textAlign="center" fontWeight="500" mr="4" flex="1">
+        Available
+      </Text>
+      <Text textAlign="center" fontWeight="500" mr="4" flex="1">
+        Borrowed
+      </Text>
+      <Text textAlign="center" fontWeight="500" mr="4" flex="1">
+        All
+      </Text>
+      <div />
+    </Grid>
+  )
+}
+
 const Loading = () => {
   return (
     <>
-      <Skeleton h="14" startColor="gray.100" endColor="gray.200" borderRadius="md" />
-      <Skeleton h="14" startColor="gray.100" endColor="gray.200" borderRadius="md" />
-      <Skeleton h="14" startColor="gray.100" endColor="gray.200" borderRadius="md" />
-      <Skeleton h="14" startColor="gray.100" endColor="gray.200" borderRadius="md" />
+      <Skeleton mb="4" h="10" mt="2" startColor="gray.100" endColor="gray.200" borderRadius="md" />
+      <Skeleton mb="4" h="10" startColor="gray.100" endColor="gray.200" borderRadius="md" />
+      <Skeleton mb="4" h="10" startColor="gray.100" endColor="gray.200" borderRadius="md" />
+      <Skeleton mb="4" h="10" startColor="gray.100" endColor="gray.200" borderRadius="md" />
     </>
   )
 }
@@ -35,15 +55,24 @@ interface ItemProps {
 const ItemItem = (props: ItemProps) => {
   const { onClickEdit, item, onClickDelete } = props
   return (
-    <Box p="4" py="2" border="1px" borderColor="gray.300" borderRadius="md">
-      <HStack spacing="0">
-        <Text fontWeight="500" mr="4" flex="1">
-          {item.title}
-        </Text>
+    <Grid templateColumns="2fr 1fr 1fr 1fr 8rem" alignItems="center">
+      <Text fontWeight="500" mr="4" flex="1">
+        {item.title}
+      </Text>
+      <Text textAlign="center" mr="4" flex="1">
+        {item.count_available || 0}
+      </Text>
+      <Text textAlign="center" mr="4" flex="1">
+        {(item.count_all || 0) - (item.count_available || 0)}
+      </Text>
+      <Text textAlign="center" fontWeight="500" mr="4" flex="1">
+        {item.count_all || 0}
+      </Text>
+      <HStack spacing="0" justifyContent="flex-end">
         <IconButton variant="ghost" aria-label="" icon={<BiTrash />} onClick={() => onClickDelete(item.id)} />
         <IconButton onClick={() => onClickEdit(item)} variant="ghost" aria-label="" icon={<BiDotsVerticalRounded />} />
       </HStack>
-    </Box>
+    </Grid>
   )
 }
 
@@ -74,14 +103,15 @@ export const ItemList = () => {
   return (
     <>
       {items && items.length === 0 && <Empty />}
-      <Grid templateColumns="repeat(4, 1fr)" gap="6">
+      <Stack divider={<StackDivider borderColor="gray.200" />}>
+        <TableHead />
         {!items && <Loading />}
         {items &&
           items.length !== 0 &&
           items.map((item) => (
             <ItemItem onClickEdit={(item) => setEdit(item)} key={item.id} item={item} onClickDelete={onClientDelete} />
           ))}
-      </Grid>
+      </Stack>
       <EditItemForm item={edit} onClose={() => setEdit(undefined)} onSave={onClientSave} />
     </>
   )
