b0955a0dcb4ae98be8a3e053e722e6ae41eef917
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2021-12-26 16:44:45 +0200

    api: items

--- api/app.py
+++ api/app.py
@@ -59,3 +59,58 @@ def delete_client(client_id):
     cursor.execute(sql, (val,))
     db.commit()
     return jsonify({'status': 'OK'}), 204
+
+
+# ITEMS
+
+@app.route('/items', methods=['POST'])
+def create_item():
+    data = request.get_json()
+    db = db_conn()
+    cursor = db.cursor()
+    sql = "INSERT INTO item (title) VALUES (%s)"
+    val = (data['title'])
+    cursor.execute(sql, (val,))
+    db.commit()
+    return jsonify({'status': 'OK'}), 201
+
+
+@app.route('/items', methods=['GET'])
+def read_items():
+    db = db_conn()
+    cursor = db.cursor()
+
+    # TODO: COUNT AVAIDABLE
+
+    cursor.execute("SELECT id, title from item")
+    db_result = cursor.fetchall()
+    cursor.close()
+    db.close()
+    payload = []
+    for row in db_result:
+        content = {'id': row[0], 'title': row[1]}
+        payload.append(content)
+    return jsonify(payload)
+
+
+@app.route('/items/<item_id>', methods=['PATCH'])
+def update_item(item_id):
+    data = request.get_json()
+    db = db_conn()
+    cursor = db.cursor()
+    sql = "UPDATE item SET title = %s WHERE id = %s"
+    val = (data['title'], item_id)
+    cursor.execute(sql, val)
+    db.commit()
+    return jsonify({'status': 'OK'}), 200
+
+
+@app.route('/items/<item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    db = db_conn()
+    cursor = db.cursor()
+    sql = "DELETE FROM item WHERE id = %s"
+    val = (item_id)
+    cursor.execute(sql, (val,))
+    db.commit()
+    return jsonify({'status': 'OK'}), 204
