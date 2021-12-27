e754bf2a4fa124fe2976657f4c013568c82edb87
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2021-12-27 11:11:43 +0200

    client: api connected

--- client/src/services/client.service.ts
+++ client/src/services/client.service.ts
@@ -1,26 +1,32 @@
 import { Client } from '../types'
-import { clients } from '../mock'
+
+const SERVER: string = 'https://garden-warehouse-api.herokuapp.com'
 
 export const getAllClients = async (): Promise<Client[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  return clients
+  const res = await fetch(`${SERVER}/clients`)
+  const data = await res.json()
+  return data as Client[]
 }
 
 export const createClient = async (client: Client): Promise<void> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  const ids: number[] = clients.map((client) => client.id)
-  client.id = Math.max(...ids) + 1
-  clients.push(client)
+  const data: any = { full_name: client.full_name }
+  await fetch(`${SERVER}/clients`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
 }
 
 export const updateClient = async (client: Client): Promise<void> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  const clientIdx: number = clients.findIndex((item) => item.id === client.id)
-  clients[clientIdx] = client
+  const { id, full_name } = client
+  const data: any = { full_name }
+  await fetch(`${SERVER}/clients/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  })
 }
 
 export const deleteClient = async (clientId: number): Promise<void> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  const clientIdx: number = clients.findIndex((item) => item.id === clientId)
-  clients.splice(clientIdx, 1)
+  await fetch(`${SERVER}/clients/${clientId}`, { method: 'DELETE' })
 }
